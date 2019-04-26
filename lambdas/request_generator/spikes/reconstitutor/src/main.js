63b6a535de94d462511afe7f4930cbb04a11e1e4
Christian Taylor <ctaylor@thoughtworks.com>
2019-04-26 17:01:18 +0100

    PRMT-246: start building the return object
            Co-authored-by: Kavita-Varsani <kvarsani@thoughtworks.com>

--- lambdas/request_generator/spikes/reconstitutor/src/main.js
+++ lambdas/request_generator/spikes/reconstitutor/src/main.js
@@ -1,3 +1,12 @@
 exports.retrieve_master_file = async () => {
-    return 'otherthings<RCMR_IN030000UK06evenmorethings<EhrExtractmorethings';
+    let content = `------=_Part_82_12073865.1555409597528
+    <eb:Manifest
+    otherthings<RCMR_IN030000UK06evenmorethings<EhrExtractmorethings`;
+
+    let name = '_Part_82_12073865.1555409597528';
+
+    return {
+        name,
+        content
+    };
 }
--- lambdas/request_generator/spikes/reconstitutor/test/when_retrieving_master_file.spec.js
+++ lambdas/request_generator/spikes/reconstitutor/test/when_retrieving_master_file.spec.js
@@ -8,10 +8,18 @@ describe('When checking my tests work', () => {
     })
 
     test("it should contain the message completed element", async () => {
-        expect(fileContent).toContain('<RCMR_IN030000UK06');
+        expect(fileContent.content).toContain('<RCMR_IN030000UK06');
     });
 
     test("it should contain the ehr extract element", async () => {
-        expect(fileContent).toContain('<EhrExtract');
+        expect(fileContent.content).toContain('<EhrExtract');
+    });
+
+    test("it should contain a manifest element", async () => {
+        expect(fileContent.content).toContain("<eb:Manifest");
+    });
+
+    test("it should have a name", async () => {
+        expect(fileContent.name).toBe('_Part_82_12073865.1555409597528');
     });
 });
