0e20c7ae23e97ad10c13f8c56e6654db3cabfd4f
Christian Taylor <ctaylor@thoughtworks.com>
2019-04-29 13:42:55 +0100

    PRMT-246: refactor
    Co-authored-by: Kavita-Varsani <kvarsani@thoughtworks.com>

--- spikes/reconstitutor/src/main.js
+++ spikes/reconstitutor/src/main.js
@@ -1,9 +1,11 @@
 exports.retrieve_master_file = async (content) => {
     
-    let name = content.match('^------=_(.*?)\n')[1];
+    let name = content.match(/^------=_(.*?)\n/)[1];
+    let files = content.match(/Filename=\"(.*?)(?=\"\s)/g); //?
 
     return {
         name,
-        content
+        content,
+        files
     };
 }
--- spikes/reconstitutor/test/when_retrieving_master_file.spec.js
+++ spikes/reconstitutor/test/when_retrieving_master_file.spec.js
@@ -1,29 +1,32 @@
 const main = require("../src/main");
 const given = require("./given");
 
-describe('When checking my tests work', () => {
-    let fileContent;
+describe('When retrieving master file', () => {
+    let fragment;
 
     beforeAll(async () => {
         let content = given.fragmentContent;
 
-        fileContent = await main.retrieve_master_file(content);
+        fragment = await main.retrieve_master_file(content);
     })
 
     test("it should contain the message completed element", async () => {
-        expect(fileContent.content).toContain('<RCMR_IN030000UK06');
+        expect(fragment.content).toContain('<RCMR_IN030000UK06');
     });
 
     test("it should contain the ehr extract element", async () => {
-        expect(fileContent.content).toContain('<EhrExtract');
+        expect(fragment.content).toContain('<EhrExtract');
     });
 
     test("it should contain a manifest element", async () => {
-        expect(fileContent.content).toContain("<eb:Manifest");
+        expect(fragment.content).toContain("<eb:Manifest");
     });
 
     test("it should have a name", async () => {
-        expect(fileContent.name).toBe('Part_82_12073865.1555409597528');
+        expect(fragment.name).toBe('Part_82_12073865.1555409597528');
     });
 
+    test("it should have 6 files", async () => {
+        expect(fragment.files.length).toBe(6);
+    });
 });
