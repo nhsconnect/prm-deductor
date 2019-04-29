c469dcca3946e66de0824023f4ed06aa355eaf95
Christian Taylor <ctaylor@thoughtworks.com>
2019-04-29 14:05:50 +0100

    PRMT-246: add file name
        Co-authored-by: Kavita-Varsani <kvarsani@thoughtworks.com>

--- spikes/reconstitutor/src/main.js
+++ spikes/reconstitutor/src/main.js
@@ -1,7 +1,14 @@
 exports.retrieve_master_file = async (content) => {
     
     let name = content.match(/^------=_(.*?)\n/)[1];
-    let files = content.match(/Filename=\"(.*?)(?=\"\s)/g); //?
+    let matches = content.match(/Filename=\"(.*?)(?=\"\s)/g);
+    let files = [];
+    matches.forEach(match => {
+        let file = {
+            name: match.slice(10)
+        };
+        files.push(file);
+    });
 
     return {
         name,
--- spikes/reconstitutor/test/when_retrieving_master_file.spec.js
+++ spikes/reconstitutor/test/when_retrieving_master_file.spec.js
@@ -29,4 +29,46 @@ describe('When retrieving master file', () => {
     test("it should have 6 files", async () => {
         expect(fragment.files.length).toBe(6);
     });
+
+    test("it should have a file called `72FA3D52-D2B2-4197-87F4-238E9C6E4AA7_Customizing a Project Plan 2013.mpp`", async () => {
+        let file = {
+            name: '72FA3D52-D2B2-4197-87F4-238E9C6E4AA7_Customizing a Project Plan 2013.mpp'
+        };
+        expect(fragment.files).toContainEqual(file);
+    });
+
+    test("it should have a file called `857419DE-7512-4619-A567-067CF9959EF1_EmisWeb.Hl7`", async () => {
+        let file = {
+            name: '857419DE-7512-4619-A567-067CF9959EF1_EmisWeb.Hl7'
+        };
+        expect(fragment.files).toContainEqual(file);
+    });
+
+    test("it should have a file called `D61AC635-4750-4118-B461-33ACC1D79478_5_06MB CommonwealthClub.ppt`", async () => {
+        let file = {
+            name: 'D61AC635-4750-4118-B461-33ACC1D79478_5_06MB CommonwealthClub.ppt'
+        };
+        expect(fragment.files).toContainEqual(file);
+    });
+
+    test("it should have a file called `3D085A2B-E00F-44F8-AA85-6699D2D4B259_(Encoded Compressed=No Length=42810092) 2003-16.tif`", async () => {
+        let file = {
+            name: '3D085A2B-E00F-44F8-AA85-6699D2D4B259_(Encoded Compressed=No Length=42810092) 2003-16.tif'
+        };
+        expect(fragment.files).toContainEqual(file);
+    });
+
+    test("it should have a file called `0D23CC94-15A3-4407-9DA8-5EF57B48581D_(Encoded Compressed=Yes Length=57347648) RAW_NIKON_D800_14bit_FX_LOSSLESS.NEF`", async () => {
+        let file = {
+            name: '0D23CC94-15A3-4407-9DA8-5EF57B48581D_(Encoded Compressed=Yes Length=57347648) RAW_NIKON_D800_14bit_FX_LOSSLESS.NEF'
+        };
+        expect(fragment.files).toContainEqual(file);
+    });
+
+    test("it should have a file called `1E49E84D-B29B-466B-93A5-432459CA549B_Treadmill Running 300fps.avi`", async () => {
+        let file = {
+            name: '1E49E84D-B29B-466B-93A5-432459CA549B_Treadmill Running 300fps.avi'
+        };
+        expect(fragment.files).toContainEqual(file);
+    });
 });
