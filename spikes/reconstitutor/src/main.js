6fd01594a8b3def3008aef8fc5082f7767d9e843
Christian Taylor <ctaylor@thoughtworks.com>
2019-04-30 09:51:53 +0100

    PRMT-246: minor tidy up
    Co-authored-by: Kavita-Varsani <kvarsani@thoughtworks.com>

--- spikes/reconstitutor/src/main.js
+++ spikes/reconstitutor/src/main.js
@@ -1,17 +1,33 @@
 const fileParser = require('./fileParser');
 
 exports.retrieve_master_file = async (content) => {
-    let name = content.match(/^------=_(.*?)\n/)[1];
+    let name = getName(content);
+    let files = getAllFiles(content);
     
-    let fileInfoCollection = content.match(/(\<eb\:Description xml\:lang=\"en\"\>Filename)(.*?)\<\/eb\:Description\>/g);
-    let files = [];
-    fileInfoCollection.forEach(fileInfo => {
-        files.push(fileParser.parseFile(fileInfo));
-    });
+    // for all attachments, add their data to the file
+
+    // for all large attachments, collate their data to the file
 
     return {
         name,
         content,
         files
     };
+}
+
+function getName(content) {
+    return content.match(/^------=_(.*?)\n/)[1];
+}
+
+function getAllFileDescriptions(content) {
+    return content.match(/(\<eb\:Description xml\:lang=\"en\"\>Filename)(.*?)\<\/eb\:Description\>/g);
+}
+
+function getAllFiles(content) {
+    let fileDescriptions = getAllFileDescriptions(content);
+    let files = [];
+    fileDescriptions.forEach(fileInfo => {
+        files.push(fileParser.parseFile(fileInfo));
+    });
+    return files;
 }
