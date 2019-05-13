4cd52ffdd939808c14dad8bdbdbc4170c8d0a67d
Dale Peakall <dale@peakall.net>
2019-05-13 11:58:42 +0100

    PRMT-251: fixed server to call lambda when receiving HTTP request.
    Co-authored-by: Cynthia Lee <leec@thoughtworks.com>

--- mtls/server/mtls_endpoint/endpoint.js
+++ mtls/server/mtls_endpoint/endpoint.js
@@ -3,27 +3,34 @@ const AWS = require('aws-sdk')
 AWS.config.update({ region: 'eu-west-2' });
 const lambda = new AWS.Lambda();
 
+const params = {
+  FunctionName: 'mtls-test-dev',
+  InvocationType: 'Event',
+};
+
+let handler = function(req, res) {
+  console.log(req.headers)
+
+  lambda.invoke(params).promise()
+    .then(() => {
+      res.statusCode = 202
+      console.log("Succeeded at calling lambda")
+      res.end();
+    })
+    .catch(err => {
+      res.statusCode = 503
+      console.log(err)  
+      res.end();
+    })
+}
+
 exports.createServer = (options) => {
   options.requestCert = true
 
-  let server = http.createServer(options, (req, res) => {
-    res.statusCode = 202
-    console.log(req.headers)
-    res.end();
-  })
+  let server = http.createServer(options, handler)
 
   server.startServer = () => {
     server.listen(options.port)
-
-    var params = {
-      FunctionName: 'mtls-test-dev',
-      InvocationType: 'Event',
-    };
-    lambda.invoke(params, function (err, data) {
-      if (err) {
-        console.log(err, err.stack)
-      }
-    });
   }
 
   return server
--- mtls/server/mtls_endpoint/main.js
+++ mtls/server/mtls_endpoint/main.js
@@ -1,7 +1,4 @@
 const endpoint = require('./endpoint')
-const AWS = require('aws-sdk')
-AWS.config.update({region: 'eu-west-2'});
-const lambda = new AWS.Lambda();
 
 let server = endpoint.createServer({
     ca: process.env.CA,
@@ -12,17 +9,6 @@ let server = endpoint.createServer({
 
 server.on('listening', () => {
     console.log("Listening on " + server.address().port)
-
-    var params = {
-        FunctionName: 'mtls-test-dev',
-        InvocationType: 'Event',
-    };
-    
-    lambda.invoke(params, function(err, data) {
-        if (err) {
-            console.log(err, err.stack)
-        }
-    });
 })
 
 server.startServer()
