afe4b52f2a4209daab176a3fcb9ccee6c2d3ee49
Christian Taylor <ctaylor@thoughtworks.com>
2019-05-13 12:26:08 +0100

    PRMT-246: build up file renamer

--- spikes/reconstitutor/src/common/fileRenamer.js
+++ spikes/reconstitutor/src/common/fileRenamer.js
@@ -1,4 +1,21 @@
-/* ignore file coverage: file system wrapper */
+const metadataExtractions = require('./metadataExtractions');
+const fs = require('fs');
+const path = require('path');
+
 exports.renameAllFilesInFolder = (folder) => {
-    console.log(folder);
+    let totalFilesRenamed = 0;
+
+    let files = fs.readdirSync(folder);
+
+    files.forEach(file => {
+        let content = fs.readFileSync(file);
+        let messageId = metadataExtractions.getMessageId(content);
+        let newFileName = path.join(folder, messageId);
+        fs.renameSync(file, newFileName);
+        totalFilesRenamed++;
+    });
+
+    return {
+        totalFilesRenamed
+    };
 }
---
+++ spikes/reconstitutor/test/when_renaming_all_files_in_folder.spec.js
@@ -0,0 +1,64 @@
+const fileRenamer = require('../src/common/fileRenamer');
+const fs = require('fs');
+jest.mock('fs');
+const path = require('path');
+const given = require('./given');
+
+describe.only('When renaming all files in a folder', () => {
+    let targetFolder, result;
+
+    beforeAll(() => {
+
+        targetFolder = 'someFolder';
+
+        fs.readdirSync = (folder) => {
+            if (folder === targetFolder) {
+                return [
+                    path.join(targetFolder, 'file1'),
+                    path.join(targetFolder, 'file2'),
+                    path.join(targetFolder, 'file3')
+                ];
+            }
+        }
+
+        fs.readFileSync = (filePath) => { 
+            switch (path.basename(filePath)) {
+                case 'file1':
+                    return given.primaryFileContent;
+                case 'file2':
+                    return given.attachmentFileContent;
+                case 'file3':
+                    return given.fragmentContent(95, 'B48B8DC1-3C90-4817-8186-E2BA3B16E2EE', 1);
+            }
+        };
+
+        fs.renameSync = jest.fn();
+
+        result = fileRenamer.renameAllFilesInFolder(targetFolder);
+    })
+
+    test('it should rename 3 files', () => {
+        expect(fs.renameSync.mock.calls.length).toBe(3);
+    });
+
+    test('it should rename the first file to its message Id', () => {
+        let expectedFullFilePath = path.join(targetFolder, '0F28A313-EEDB-413E-9D41-BED8213DCB95');
+        expect(fs.renameSync.mock.calls[0][1]).toBe(expectedFullFilePath);
+    });
+
+    test('it should rename the second file to its message Id', () => {
+        let expectedFullFilePath = path.join(targetFolder, 'E5EE718C-2577-401B-AFC3-CB651FD3011F');
+        expect(fs.renameSync.mock.calls[1][1]).toBe(expectedFullFilePath);
+    });
+
+    test('it should rename the third file to its message Id', () => {
+        let expectedFullFilePath = path.join(targetFolder, 'B48B8DC1-3C90-4817-8186-E2BA3B16E2EE');
+        expect(fs.renameSync.mock.calls[2][1]).toBe(expectedFullFilePath);
+    });
+
+    test('it should return a detailed report', () => {
+        expect(result).toEqual({
+            totalFilesRenamed: 3
+        });
+    });
+});
